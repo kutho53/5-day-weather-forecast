1722aa8df7622b52f7d5d4973fd7ba18d35a453e
kutho53 <kurtisthomas53@gmail.com>
2025-01-04 23:39:08 -0500

    historyService

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -1,12 +1,29 @@
 // TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: string;
+  constructor(name: string, id: string){
+    this.name = name;
+    this.id = id;
+  }
+}
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read(): Promise<City[]> {
+    try {
+      const data = await fstat.readFile('db/db.json', 'utf8');
+      const cities: City[] = JSON.parse(data);
+      return cities;
+    } catch (error) {
+      console.error('Error reading search history: ', error);
+      return [];
+    }
+  }
 
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(cities: City[]): Promise {}
 
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   // async getCities() {}

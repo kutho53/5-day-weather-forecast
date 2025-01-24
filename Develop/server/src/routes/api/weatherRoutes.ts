40b9e31a71e5b87e32ca4b0ee9d26e9d35ea301b
kutho53 <kurtisthomas53@gmail.com>
2025-01-23 22:34:00 -0500

    search history

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -4,6 +4,7 @@ const router = Router();
 import HistoryService from '../../service/historyService.js';
 
 import WeatherService from '../../service/weatherService.js';
+import historyService from '../../service/historyService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req, res) => {
@@ -13,7 +14,15 @@ router.post('/', (req, res) => {
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (req, res) => {
+  try{
+    const searchHistory = await historyService.getCities();
+    res.status(200).json(searchHistory);
+  } catch (error){
+    console.error('cannot retrieve search history', error);
+    res.status(500).json({error: 'An error occured while fetching searchHistory'})
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req, res) => {});
